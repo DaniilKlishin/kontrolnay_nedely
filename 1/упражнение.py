10c1d70a4964e05e2f4c92731d1af55ede1cff18
daniil <klishin_0606@mail.ru>
2023-09-19 21:32:52 +0700

    сделал первое задание

---
+++ 1/упражнение.py
@@ -0,0 +1,9 @@
+import math
+
+a= float(input('Введите a: '))
+
+z1= math.cos(3/8*math.pi - a/4)**2 - math.cos(11/8*math.pi + a/4)
+z2= (math.sqrt(2)/2)* math.sin(a/2)
+
+print (f'z1 = {z1}')
+print (f'z2 = {z2}')
